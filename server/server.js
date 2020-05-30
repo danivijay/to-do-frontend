29966cb4487e5aa5f9e4855c3d77bcca503992f4
danivijay <danimvijay@gmail.com>
2020-05-30 15:48:18 +0530

    feat: add server raw code

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const colors = require("colors");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
+
+require("dotenv").config({ path: "./config/config.env" });
+
+connectDB();
+
+const todoRoutes = require("./routes/todo");
+const userRoutes = require("./routes/users");
+
+const app = express();
+app.use(express.json());
+
+app.use("/api/v1/auth", userRoutes);
+app.use("/api/v1/todo", todoRoutes);
+
+const PORT = process.env.PORT;
+app.listen(
+  PORT,
+  console.log(
+    `Server Running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
