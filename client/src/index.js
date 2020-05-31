28a737a4b916e971955d52b6a5a2b172bb4bf5ed
danivijay <danimvijay@gmail.com>
2020-05-31 22:12:10 +0530

    feat(fe): add redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import reducer from "reducers";
+
+import "./index.css";
+const store = createStore(
+  reducer,
+  compose(
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(thunk)
+  )
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
